8865ebf5771ddf16c3944f9b07ed9d414d9b7332
felgamedev <felgamedev@gmail.com>
2018-03-27 09:40:22 -0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,114 @@
+var ENEMIES = 3;
+
+var TILE_WIDTH = 101;
+var TILE_HEIGHT = 171;
+
+var worldGrid = [5][6];
+// Distance of 83px between tile rows
+var enemyYPositions = [65, 148, 231];
+
+function randomEnemyRow(){
+  return Math.floor(Math.random() * enemyYPositions.length);
+}
+
+function randomXOffset(){
+  return -TILE_WIDTH - (Math.random() * 150);
+}
+
+function randomPlayerYTile(){
+  return Math.floor(Math.random() * 6);
+}
+
+function randomSpeed(){
+  return 100 + Math.floor(Math.random() * 100);
+}
+
+// Enemies our player must avoid
+var Enemy = function(xSpeed, xStartLocation, yLocation) {
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.speed = xSpeed;
+  this.x = xStartLocation;
+  this.y = yLocation;
+  this.row;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += dt * this.speed;
+
+    if(this.x >= 505){
+      this.x = randomXOffset();
+      this.y = enemyYPositions[randomEnemyRow()];
+      this.speed = randomSpeed();
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+var Player = function(gridX, gridY) {
+  this.sprite = 'images/char-boy.png';
+  this.gridX = gridX;
+  this.gridY = gridY;
+
+  this.x = this.gridX * TILE_WIDTH;
+  this.y = (this.gridY * 83) - 35;
+}
+
+Player.prototype.update = function(dt){}
+
+Player.prototype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.gridX * TILE_WIDTH, (this.gridY * 83) - 35);
+}
+
+Player.prototype.handleInput = function(e){
+  if(e == 'left') this.gridX--;
+  if(e == 'right') this.gridX++;
+  if(e == 'up') this.gridY--;
+  if(e == 'down') this.gridY++;
+  if(this.gridX < 0) this.gridX = 0;
+  if(this.gridX > 4) this.gridX = 4;
+  if(this.gridY < 0) this.gridY = 0;
+  if(this.gridY > 5) this.gridY = 5;
+}
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+for(let i = 0; i < ENEMIES; i++){
+  allEnemies.push(new Enemy(randomSpeed(), randomXOffset(), enemyYPositions[randomEnemyRow()]));
+}
+var player = new Player(3, 3);
+
+
+
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
