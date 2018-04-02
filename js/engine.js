22dd848b3c50d73fb40c9f84e134d0ee2d9c1653
felgamedev <felgamedev@gmail.com>
2018-04-02 19:58:20 -0300

    Added image files for updated bugs

--- js/engine.js
+++ js/engine.js
@@ -178,7 +178,9 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/enemy-bug-faster.png',
+        'images/enemy-bug-fastest.png'
     ]);
     Resources.onReady(init);
 
